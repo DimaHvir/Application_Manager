95dea39421cd4d3b3dcedb0fc25d00846945e07b
DimaHvir <dimahvirtsman@gmail.com>
2022-04-04 17:17:38 -0400

    fixed UI issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,16 @@ import './App.css';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import {Authenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(awsconfig);
+
+
 
 function App() {
   return (
     <Authenticator>
-    {({ signOut, user }) => (
+    {({signOut , user }) => (
         <main>
           <h1>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
