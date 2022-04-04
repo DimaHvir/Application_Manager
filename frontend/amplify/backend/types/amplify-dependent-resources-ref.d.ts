af1e67c9484f4bee785c95cbef4b063adb233c5d
DimaHvir <dimahvirtsman@gmail.com>
2022-04-04 13:49:21 -0400

    Project intialized; Amplify Auth added

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "frontend8dd3a79e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
